155c34aab885fccf30fd6990ea6ab3a8a4160f73
rnoguer22 <rnogugon@myuax.com>
2024-02-03 11:54:10 +0100

    Obtenemos el nombre de las tablas de bookmaker.db

--- Analisis_Datos/analisis.py
+++ Analisis_Datos/analisis.py
@@ -4,6 +4,21 @@ import pandas as pd
 # Conectarse a la base de datos SQLite
 conn = sqlite3.connect('Api_Casas_Apuestas-main/bookmaker.db')
 
+# Crear un cursor para ejecutar consultas SQL
+cursor = conn.cursor()
+
+# Ejecutar una consulta para obtener los nombres de las tablas
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+# Obtener los resultados de la consulta
+tables = cursor.fetchall()
+tables_list = []
+
+# Obtenemos los nombres de las tablas
+for table in tables:
+    tables_list.append(table[0])
+
+
 # Ejecutar una consulta SQL y cargar los resultados en un DataFrame de Pandas
 df = pd.read_sql_query("SELECT * FROM bookmaker.db", conn)
 
