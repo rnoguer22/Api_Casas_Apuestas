4a3b377e6c21f0793594428442bf9493a04c234f
rnoguer22 <rnogugon@myuax.com>
2024-02-10 21:07:41 +0100

    Extraemos los datos de la web a un archivo html

--- Web_Scrapping/scrapper.py
+++ Web_Scrapping/scrapper.py
@@ -2,6 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-r = requests.get('https://www.sofascore.com/tournament/football/europe/uefa-champions-league/7')
-soup = BeautifulSoup(r.text, 'xml')
-print(soup)
+
+# URL de la página a analizar
+url = 'https://fbref.com/en/comps/8/Champions-League-Stats' 
+
+# Realizar una solicitud GET para obtener el HTML de la página
+response = requests.get(url)
+
+# Verificar si la solicitud fue exitosa (código de estado 200)
+if response.status_code == 200:
+    # Obtener el contenido HTML de la respuesta
+    html_content = response.content
+
+    # Crear un objeto BeautifulSoup para analizar el HTML
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Encontrar y extraer el contenido del cuerpo de la página
+    body_content = soup.find('body')
+
+    # Convertir el objeto Tag a una cadena usando prettify()
+    body_str = body_content.prettify()
+
+    # Imprimir el contenido del cuerpo de la página
+    with open('.\Web_Scrapping\data.html', 'w', encoding='utf-8') as file:
+        file.write(body_str)
+
+else:
+    print('Error al obtener el contenido de la página:', response.status_code)
