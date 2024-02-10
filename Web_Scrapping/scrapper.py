e9e50e0a7f003adbe31750714dbd233cbe41096b
rnoguer22 <rnogugon@myuax.com>
2024-02-10 19:01:34 +0100

    Vamos a hacer un poquito de web scrapping

---
+++ Web_Scrapping/scrapper.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+r = requests.get('https://www.sofascore.com/tournament/football/europe/uefa-champions-league/7')
+soup = BeautifulSoup(r.text, 'lxml')
