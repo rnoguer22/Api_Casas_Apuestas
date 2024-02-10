eee7138bd189d89940f34d18363adabc21fc8654
rnoguer22 <rnogugon@myuax.com>
2024-02-10 21:30:40 +0100

    Obtenemos los nombres de las columnas

--- Web_Scrapping/scrapper.py
+++ Web_Scrapping/scrapper.py
@@ -23,9 +23,23 @@ if response.status_code == 200:
     # Convertir el objeto Tag a una cadena usando prettify()
     body_str = body_content.prettify()
 
+    '''
     # Imprimir el contenido del cuerpo de la página
     with open('.\Web_Scrapping\data.html', 'w', encoding='utf-8') as file:
         file.write(body_str)
+    '''
 
 else:
     print('Error al obtener el contenido de la página:', response.status_code)
+
+
+overall_stats = soup.find('div', id='div_results2023-202480_overall')
+print(overall_stats.prettify())
+
+soup_overall = BeautifulSoup(str(overall_stats), 'html.parser')
+col = soup_overall.find_all('th', scope='col')
+columns = []
+for th in col:
+    content = th.get_text(strip=True)
+    columns.append(content)
+print(columns)
