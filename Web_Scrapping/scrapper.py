1d7bc89a4438aa9f56161453af88798c35f1bc24
rnoguer22 <rnogugon@myuax.com>
2024-02-11 17:51:52 +0100

    Metodo para obtener el html de una web

--- Web_Scrapping/scrapper.py
+++ Web_Scrapping/scrapper.py
@@ -8,31 +8,25 @@ class Scrapping:
     def __init__(self, url) -> None:
         self.url = url
 
-# Realizar una solicitud GET para obtener el HTML de la página
-response = requests.get(url)
 
-# Verificar si la solicitud fue exitosa (código de estado 200)
-if response.status_code == 200:
-    # Obtener el contenido HTML de la respuesta
-    html_content = response.content
-
-    # Crear un objeto BeautifulSoup para analizar el HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Encontrar y extraer el contenido del cuerpo de la página
-    body_content = soup.find('body')
-
-    # Convertir el objeto Tag a una cadena usando prettify()
-    body_str = body_content.prettify()
-
-    '''
-    # Imprimir el contenido del cuerpo de la página
-    with open('.\Web_Scrapping\data.html', 'w', encoding='utf-8') as file:
-        file.write(body_str)
-    '''
-
-else:
-    print('Error al obtener el contenido de la página:', response.status_code)
+    #Metodo para obtener el HTML de la web
+    def get_html(self, write:bool=False, path:str=None):
+        # Realizar una solicitud GET para obtener el HTML de la página
+        response = requests.get(self.url)
+        # Verificar si la solicitud fue exitosa (código de estado 200)
+        if response.status_code == 200:
+            # Obtener el contenido HTML de la respuesta
+            html_content = response.content
+            # Crear un objeto BeautifulSoup para analizar el HTML
+            self.soup = BeautifulSoup(html_content, 'html.parser')
+            if write:
+                # Convertir el objeto Tag a una cadena usando prettify()
+                html_str = self.soup.prettify()
+                # Imprimir el contenido del cuerpo de la página
+                with open(path, 'w', encoding='utf-8') as file:
+                    file.write(html_str)
+        else:
+            print('Error al obtener el contenido de la página:', response.status_code)
 
 
 overall_stats = soup.find('div', id='div_results2023-202480_overall')
