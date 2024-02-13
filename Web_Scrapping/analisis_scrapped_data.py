9b7a178bd012f706f912a9fa4c48ca62982d4fb2
rnoguer22 <rnogugon@myuax.com>
2024-02-13 18:35:41 +0100

    Filtramos los datos para obtener la informacion que nos interesa

--- Web_Scrapping/analisis_scrapped_data.py
+++ Web_Scrapping/analisis_scrapped_data.py
@@ -9,11 +9,20 @@ class AnalisisScrappedData:
         self.rutas = os.listdir(self.ruta)
 
 
+    #En esta funcion vamos a analizar los datos de los csv
     def analize_csv(self):
         for csv in self.rutas:
-            data_path = self.ruta + '/' + csv
-            self.analize(data_path)
-    
+            self.data_path = self.ruta + '/' + csv
+            self.analize(self.data_path)
+
+    #En esta funcion vamos a analizar los datos de un dataframe
+    def analize(self, path):
+        df = pd.read_csv(path)
+        df.drop('Notes', inplace=True, axis=1)
+        df.drop('Top Team Scorer', inplace=True, axis=1)
+        df.drop('Goalkeeper', inplace=True, axis=1)
+        df.dropna(inplace=True)
+        df.to_csv(path, index=False)
 
 
 
