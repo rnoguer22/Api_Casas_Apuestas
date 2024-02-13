f1e150aadb87e89f9e70ee620918349a1ceddc6f
rnoguer22 <rnogugon@myuax.com>
2024-02-13 18:28:10 +0100

    Metodo analize_csv para analizar cada uno de los csv scrappeados

--- Web_Scrapping/analisis_scrapped_data.py
+++ Web_Scrapping/analisis_scrapped_data.py
@@ -1,9 +1,23 @@
 import os
+import pandas as pd
+
 
 class AnalisisScrappedData:
 
     def __init__(self) -> None:
-        self.rutas = os.listdir('Web_Scrapping/scrapped_csv')
+        self.ruta = 'Web_Scrapping/scrapped_csv'
+        self.rutas = os.listdir(self.ruta)
+
+
+    def analize_csv(self):
+        for csv in self.rutas:
+            data_path = self.ruta + '/' + csv
+            self.analize(data_path)
+    
+
+
+
 
 analisis = AnalisisScrappedData()
 print(analisis.rutas)
+analisis.analize_csv()
